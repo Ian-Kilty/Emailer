db1d0864d986d2db94b9d2b2d95b826b9c1fd123
Ian Kilty <iankilty1@gmail.com>
2022-04-20 17:50:53 -0600

    file based input

--- mailer.py
+++ mailer.py
@@ -1,23 +1,40 @@
+# Usage python mailer.py <file> -> email, subject, var1, var2... <file> -> body text with variables defined with <>
+# body.txt Ex. Hello {data[0]}, how are you doing this data[1]? -> must be one line
+# {data[0]} is sent as the variable put in var1 in the arguments
+
+# Make sure your email accout is authorized with https://myaccount.google.com/lesssecureapps
+
+email = input("Email: ")
+password = input("Password: ")
+import sys
 import smtplib
 from email.message import EmailMessage
 emails = []
 subjects = []
 bodys = []
-f = open("emails.txt", "r")
+template = ""
+body = ""
+f = open(str(sys.argv[1]), "r")
 for x in f:
-  emails.append(x.split(",")[0])
-  bodys.append(x.split(",")[1])
-  subjects.append(x.split(",")[2].strip())
+  x = x.split(",")
+  data = x[2:len(x)]
+  template = open(sys.argv[2], "r").read()
+  body = template
+  for i in range(len(data)):
+    body = body[:body.find("{")] + data[i].strip() + body[body.find("}")+1:]
+  emails.append(x[0])
+  bodys.append(body)
+  subjects.append(x[1].strip())
 for i in range(len(emails)):
     msg = EmailMessage()
-    subject = subjects[i] + " craigslist"
+    subject = subjects[i]
     msg['Subject'] = subject
-    msg['From'] = 'iankilty98@gmail.com'
+    msg['From'] = email
     msg['To'] = emails[i]
-    body = "Hey my name is Ian and I saw your ad on craigslist for the " + bodys[i] + " and I was wondering if it is still available and if so if I can come pick it up? \n\nThank you!"
-    msg.set_content(body)
+    body1 = body
+    msg.set_content(body1)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login('iankilty98@gmail.com', 'Office365Yahoo')
+        smtp.login(email, password)
         smtp.send_message(msg)
         print("Sent to: " + emails[i] + " : " + subjects[i])
